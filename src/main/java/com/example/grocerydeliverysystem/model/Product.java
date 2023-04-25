b3a6e307cbfe4b63510a7b9f69cb297f2b690209
benj302g <benj302g@stud.kea.dk>
2023-04-25 13:19:06 +0200

    init commit

--- src/main/java/com/example/grocerydeliverysystem/model/Product.java
+++ src/main/java/com/example/grocerydeliverysystem/model/Product.java
@@ -1,2 +1,35 @@
-package com.example.grocerydeliverysystem.model;public class Product {
+package com.example.grocerydeliverysystem.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+// Lombok constructors og setters/getters.
+// JPA skal altid bruge en tom constructor
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // Vi bruger ikke GenerationType.AUTO, fordi den laver en hjælpetabel der viser, hvad det næste ID er.
+    // Det gør IDENTITY ikke. Man kan self godt, men vi skal ikke bruge tabellen til noget.
+    private long id;
+
+    private String name;
+    private double price;
+    private double weight;
+
+
+
+
 }
